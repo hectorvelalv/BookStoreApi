0cb559914eb88aab2bf09b65b1f9b66a35b1c880
hvelasco <hvelasco@griddynamics.com>
2023-03-10 11:15:20 -0600

    removing redundant code (ResponseEntity) from controller

--- src/main/java/com/bookStore/demo/controllers/StoreController.java
+++ src/main/java/com/bookStore/demo/controllers/StoreController.java
@@ -3,8 +3,6 @@ package com.bookStore.demo.controllers;
 import com.bookStore.demo.models.dtos.BookDto;
 import com.bookStore.demo.models.entities.Book;
 import com.bookStore.demo.services.implementations.BookServiceImpl;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,17 +18,17 @@ public class StoreController {
     }
 
     @PostMapping("/addBook")
-    public ResponseEntity<BookDto> addBook(@RequestBody Book book) {
-        return new ResponseEntity<>(bookService.addBook(book), HttpStatus.OK);
+    public BookDto addBook(@RequestBody Book book) {
+        return bookService.addBook(book);
     }
 
     @GetMapping("/getBook/{id}")
-    public ResponseEntity<BookDto> getBookById(@PathVariable(value = "id") final Long id) {
-        return new ResponseEntity<>(bookService.getBookById(id), HttpStatus.OK);
+    public BookDto getBookById(@PathVariable(value = "id") final Long id) {
+        return bookService.getBookById(id);
     }
     @GetMapping("/getAllBooks")
-    public ResponseEntity<List<BookDto>> getAllBooks() {
-        return new ResponseEntity<>(bookService.getAllBooks(), HttpStatus.OK);
+    public List<BookDto> getAllBooks() {
+        return bookService.getAllBooks();
     }
 
     @DeleteMapping("deleteBook/{id}")
@@ -39,7 +37,7 @@ public class StoreController {
     }
 
     @PutMapping("editBook/{id}")
-    public ResponseEntity<BookDto> editBook(@PathVariable(value = "id") final Long id, @RequestBody final Book editedBook){
-        return new ResponseEntity<>(bookService.updateBook(id, editedBook),HttpStatus.OK);
+    public BookDto editBook(@PathVariable(value = "id") final Long id, @RequestBody final Book editedBook){
+        return bookService.updateBook(id, editedBook);
     }
 }
